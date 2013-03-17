439435933d433fc04dfd89f197a205deb6ebfede
= <=>
2013-03-17 13:23:55 +0800

    null

---
+++ protected/modules/rights/views/_menu.php
@@ -0,0 +1,64 @@
+<?php /*$this->widget('zii.widgets.CMenu', array(
+	'firstItemCssClass'=>'first',
+	'lastItemCssClass'=>'last',
+	'htmlOptions'=>array('class'=>'actions'),
+	'items'=>array(
+		array(
+			'label'=>Rights::t('core', 'Assignments'),
+			'url'=>array('assignment/view'),
+			'itemOptions'=>array('class'=>'item-assignments'),
+		),
+		array(
+			'label'=>Rights::t('core', 'Permissions'),
+			'url'=>array('authItem/permissions'),
+			'itemOptions'=>array('class'=>'item-permissions'),
+		),
+		array(
+			'label'=>Rights::t('core', 'Roles'),
+			'url'=>array('authItem/roles'),
+			'itemOptions'=>array('class'=>'item-roles'),
+		),
+		array(
+			'label'=>Rights::t('core', 'Tasks'),
+			'url'=>array('authItem/tasks'),
+			'itemOptions'=>array('class'=>'item-tasks'),
+		),
+		array(
+			'label'=>Rights::t('core', 'Operations'),
+			'url'=>array('authItem/operations'),
+			'itemOptions'=>array('class'=>'item-operations'),
+		),
+	)
+));*
+$this->menu=array(
+		array(
+			'label'=>Rights::t('core', 'Assignments'),
+			'url'=>array('assignment/view'),
+			'itemOptions'=>array('class'=>'item-assignments'),
+			'icon'=>'isw-favorite'
+		),
+		array(
+			'label'=>Rights::t('core', 'Permissions'),
+			'url'=>array('authItem/permissions'),
+			'itemOptions'=>array('class'=>'item-permissions'),
+			'icon'=>'isw-lock'
+		),
+		array(
+			'label'=>Rights::t('core', 'Roles'),
+			'url'=>array('authItem/roles'),
+			'itemOptions'=>array('class'=>'item-roles'),
+			'icon'=>'isw-user'
+		),
+		array(
+			'label'=>Rights::t('core', 'Tasks'),
+			'url'=>array('authItem/tasks'),
+			'itemOptions'=>array('class'=>'item-tasks'),
+			'icon'=>'isw-graph'
+		),
+		array(
+			'label'=>Rights::t('core', 'Operations'),
+			'url'=>array('authItem/operations'),
+			'itemOptions'=>array('class'=>'item-operations'),
+			'icon'=>'isw-pin'
+		),
+	);*/	?>
